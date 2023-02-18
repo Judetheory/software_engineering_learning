4a8523e7f720d22500d2a589f85a3bf1a8a31c62
Jude Tochukwu Ogbozor <judtheory@gmail.com>
2023-02-18 14:23:18 -0800

    having input capability using scanf

---
+++ C_programming/input.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+	int radius;
+	printf("Enter the radius of the circle: ");
+	scanf("%i\n", &radius);
+	printf("The value of the radius is %i:", radius);
+	return (0);
+}
