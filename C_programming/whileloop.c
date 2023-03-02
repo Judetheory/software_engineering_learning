8b24161026376b8d8697c558f71ff6916c47d4b6
Jude Tochukwu Ogbozor <judtheory@gmail.com>
2023-03-02 12:44:02 -0800

    learning basic array

---
+++ C_programming/whileloop.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+	int i = 0;
+	
+	while(i <= 10)
+	{
+		int k;
+
+		while(k = i)
+			while(i != 0)
+			{
+
+			printf("%i\n", k);
+			k--;
+			}
+	}
+	return 0;
+		
+}
