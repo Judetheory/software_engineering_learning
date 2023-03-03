86d4b64e9936efc2d0347d3fff7f4eccfccf23d1
Jude Tochukwu Ogbozor <judtheory@gmail.com>
2023-03-03 14:24:49 -0800

    function primary

---
+++ C_programming/function.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int factorial(int number)
+{
+	int factorial = 1;
+	
+	for (int i = number; i > 1; i--)
+	{
+		factorial *= 1;
+	return factorial;
+	}
+	printf("The factorial of %d is %d \n", number, factorial);		
+}
